1c684d5aae6f6078156506791608c331117c8e7b
KesslerBarreto <kesslergremio@gmail.com>
2024-06-20 20:49:11 -0300

    exercicio de retornar texto em caixa alta

---
+++ caixa-alta.js
@@ -0,0 +1,17 @@
+//Aprendendo a usar caixa alta 
+function retornaCaixaAlta(texto) {
+    return texto.toUpperCase();
+}
+
+let texto = 'Bom dia'; 
+let meuNome = "Kessler";
+let nomeDaMae = "Josiane";
+
+console.log(retornaCaixaAlta(texto));
+console.log("------------");
+
+console.log(retornaCaixaAlta(meuNome));
+console.log("------------");
+
+console.log(retornaCaixaAlta(nomeDaMae));
+console.log("------------");
