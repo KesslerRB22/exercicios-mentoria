be531d7bb1ca09d32bfef46285c6c1fd4f41127c
KesslerBarreto <kesslergremio@gmail.com>
2024-06-24 20:33:13 -0300

    exercicio de return

---
+++ exercicios-return.js
@@ -0,0 +1,8 @@
+let listaDeNúmero = 9[1, 2, 3, 4, 5, 6, 7, 8];
+
+function filtrarNumeros(numeros){
+    return numeros > 3;
+
+
+let numerosMenoresQueTres = listaDeNúmero.filter(numerosMenoresQueTres);
+console.log(filtrarNumeros(listaDeNúmero > 3 ));
