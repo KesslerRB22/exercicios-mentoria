3e189f68ff63636d556206e69b531e3e996d530b
KesslerBarreto <kesslergremio@gmail.com>
2024-06-24 20:57:10 -0300

    Praticando If Else e Else If

---
+++ programa-boletim-nota-A.js
@@ -0,0 +1,17 @@
+//Testando if else e else if 
+
+let notaDoEstudante = 2; 
+
+if (notaDoEstudante >= 8){
+    console.log("Nota A"); 
+} else if (notaDoEstudante >=6 && notaDoEstudante >= 8){
+    console.log("Nota B");
+} else if (notaDoEstudante >=4 && notaDoEstudante > 6){
+    console.log("Nota C"); 
+} else {
+    console.log("Nota D");
+} 
+console.log(notaDoEstudante); 
+
+
+
